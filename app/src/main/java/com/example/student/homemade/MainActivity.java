8bff9ee25f32e052c27ff5a8022d4168d700cd3a
Ab261 <avakash261@gmail.com>
2019-03-08 15:25:14 +0530

    Added dynamic image and email to navigation drawer

--- app/src/main/java/com/example/student/homemade/MainActivity.java
+++ app/src/main/java/com/example/student/homemade/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,12 +19,14 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.student.homemade.ui.ConsumerDetailsFragment;
@@ -34,8 +39,12 @@ import com.example.student.homemade.ui.TrendingItemsFragment;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity implements ProviderUIFragment.OnFragmentInteractionListener, ConsumerUIFragment.OnFragmentInteractionListener{
     private static int c;
@@ -64,6 +73,7 @@ public class MainActivity extends AppCompatActivity implements ProviderUIFragmen
         Log.v(TAG,settings.getString("email","homemade"));
         c = settings.getInt("ProviderOrConsumerFlag", 0);
         navigationView = findViewById(R.id.nav_view);
+
         Menu menu = navigationView.getMenu();
         menu.add(0, 0, 0, "Dashboard");
 
@@ -107,8 +117,31 @@ public class MainActivity extends AppCompatActivity implements ProviderUIFragmen
         };
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawer.setDrawerListener(mDrawerToggle);
-        navigationView = findViewById(R.id.nav_view);
         setupDrawerContent(navigationView);
+        View hView = navigationView.inflateHeaderView(R.layout.nav_header);
+        final ImageView imageView = hView.findViewById(R.id.header_imageView);
+        StorageReference mImageRef =
+                FirebaseStorage.getInstance().getReference("consumers_photos/image:48110");
+        final long ONE_MEGABYTE = 1024 * 1024;
+        mImageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                DisplayMetrics dm = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+                imageView.setMinimumHeight(dm.heightPixels);
+                imageView.setMinimumWidth(dm.widthPixels);
+                imageView.setImageBitmap(bm);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+        TextView headertextView = hView.findViewById(R.id.username_header);
+        headertextView.setText(settings.getString("email","homemade"));
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -188,9 +221,6 @@ public class MainActivity extends AppCompatActivity implements ProviderUIFragmen
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    private void setupDrawer() {
-
-    }
     private void setupDrawerContent(NavigationView navigationView) {
 
 
