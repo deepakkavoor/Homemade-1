864f09819f43692a67215b54bd1afd944eb69a26
govardhangdg <42682845+govardhangdg@users.noreply.github.com>
2019-03-12 08:42:03 +0530

    Govardhan (#33)
    * changes to availability
    
    * commiting my code
    
    * commiting my changes 2
    
    * my  changes 3
    
    * pulled the changes
    
    * changes to UI and added orderDate
    
    * resolved merge errors

--- app/src/main/java/com/example/student/homemade/CustomerConfirmActivity.java
+++ app/src/main/java/com/example/student/homemade/CustomerConfirmActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.HashMap;
+
 
 
 public class CustomerConfirmActivity extends AppCompatActivity {
@@ -61,6 +65,8 @@ public class CustomerConfirmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer_confirm);
         FirebaseApp.initializeApp(this);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Confirm Order");
         Intent intent = getIntent();
         final String doc = intent.getStringExtra("docRef");
         Log.d("docRef",doc);
