1919ef61ad9ec96a3d929ea708fca1fdbddd1397
NishanthSubramanian <nishanth80442@gmail.com>
2019-03-12 07:21:44 +0530

    Bugs fixed

--- app/src/main/java/com/example/student/homemade/UploadActivity.java
+++ app/src/main/java/com/example/student/homemade/UploadActivity.java
@@ -412,6 +412,9 @@ else{
         Intent intent = new Intent(this, MenuActivity.class);
         Log.d("KJSFKJSDHFKJSDHFKJSDHF",type);
         intent.putExtra("type", type);
+        intent.putExtra("seller", seller);
+        intent.putExtra("present",present);
+        intent.putExtra("absent",absent);
         intent.putExtra("menuItems",menuItems);
         intent.putExtra("itemPictures",itemPictures);
         startActivity(intent);
