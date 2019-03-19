0272cf30c486425d68d34054178eafd81ff4113f
deepakkavoor <31254259+deepakkavoor@users.noreply.github.com>
2019-03-19 21:33:19 +0530

    PR from deepak (#73)
    * Update README.md
    
    * db for cancel
    
    * report
    
    * report
    
    * improved ui
    
    * committed
    
    * changed cancel stuff
    
    * made cancel order changes
    
    * fixed db error
    
    * made wallet changes
    
    * changed db

--- app/src/main/java/com/example/student/homemade/CancelOrderFragment.java
+++ app/src/main/java/com/example/student/homemade/CancelOrderFragment.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import com.example.student.homemade.ui.HistoricalOrdersFragment;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
